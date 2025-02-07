dec2f2decdf906c5d0329d5dfd2233f720f58d83
shellew <nishiokaryouichi2020@MacBook-Air.local>
2025-02-07 16:19:20 +0900

    app.runのhostとportの修正

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def serve_sw():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
